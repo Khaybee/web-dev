804e716e9be912d6d411a7c8516d609cf196b8dd
Khaybee <kachieosuji11@gmail.com>
2024-02-03 11:53:20 +0100

    added javascript function to library

--- Library/jav.js
+++ Library/jav.js
@@ -1,1 +1,8 @@
-alert("Hello there")
+let btns = document.querySelectorAll("#book-list .delete")
+
+btns.forEach(function (btn) {
+     btn.addEventListener('click', function(e) {
+          const li = e.target.parentElement
+          li.parentNode.removeChild(li)
+     })
+})
